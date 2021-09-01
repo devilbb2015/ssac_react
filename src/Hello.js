d9b7f5d0aac30fa78d42e199e59f0b88b8bf3a60
devilb2015 <devilb2015@gmail.com>
2021-09-01 09:21:41 +0900

    프로젝트 생성 및 실습

---
+++ src/Hello.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Hello() {
+    let name = 'React';
+
+    return(
+        <>
+            <section>
+                Hello {name} in Hello Component
+            </section>
+            <section>
+                Hello {name} in Hello Component
+            </section>
+        </>
+
+    );
+
+}
+
+export default Hello;

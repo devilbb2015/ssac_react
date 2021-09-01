ab200ebdbf311b3266cd0559c6a1f07348c80d7b
devilb2015 <devilb2015@gmail.com>
2021-09-01 18:01:44 +0900

    수업정리 및 실습

---
+++ src/component/TodoMain.js
@@ -0,0 +1,13 @@
+import main from '../css/main.module.css';
+
+function TodoMain({children}) {
+    return (
+        <main className={main.main}>
+            {children}
+        
+        </main>
+    );
+
+};
+
+export default TodoMain;
